51767e10fb1b6d5f9c243f6471984335fca892ff
Vedant <d22dcs175@charusat.edu.in>
2023-07-12 10:11:12 +0530

    Changes

---
+++ prac2/src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css';
+import Home from './Components/Home/Home';
+import Contact from './Components/Contact/Contact';
+import About from './Components/About/About';
+
+function App() {
+  return (
+    <div>
+   <BrowserRouter>
+   <Routes>
+    <Route exact path="/Home" element={<Home/>}/>
+    <Route exact path="/About" element={<About/>}/>
+    <Route exact path="/Contact" element={<Contact/>}/>
+    </Routes>
+    </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
